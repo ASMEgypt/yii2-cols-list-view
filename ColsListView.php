e9885bfaea70f6ffd665fc1b34d43d21516cee71
eXeCUT <sexecut@gmail.com>
2019-06-25 16:01:27 +0300

    Added dynamic columns

--- ColsListView.php
+++ ColsListView.php
@@ -12,10 +12,11 @@ use yii\helpers\Html;
 
 class ColsListView extends \yii\widgets\ListView
 {
-    protected $currentCount = 0;
     public $textAttribute = 'name';
     public $itemTemplate = '{item}';
     public $valAttribute = 'id';
+    protected $currentCount = 0;
+    protected $isStaticColumns = false;
     public function renderItems() {
         return parent::renderItems();
     }
@@ -31,29 +32,38 @@ class ColsListView extends \yii\widgets\ListView
     public function renderItem($row, $key, $index) {
         $dataProvider = $this->dataProvider;
         $textAttribute = $this->textAttribute;
-        $content = $this->renderItemValue($row, $key, $index);
-        $result = '';
-        $partSize = ceil(count($dataProvider->models) / 3);
-        if ($index == 0) {
-            $result .= '<div class="col-sm-4">';
-            $this->currentCount++;
-        } else if ($this->currentCount == $partSize) {
-            $result .= '</div><div class="col-sm-4">';
-            $this->currentCount = 1;
-        } else {
-            $this->currentCount++;
-        }
-
         $valAttribute = $this->valAttribute;
+        $content = $this->renderItemValue($row, $key, $index);
+        if ($this->isStaticColumns) {
+            $result = '';
+            $partSize = ceil(count($dataProvider->models) / 3);
+            if ($index == 0) {
+                $result .= '<div class="col-sm-4">';
+                $this->currentCount++;
+            } else if ($this->currentCount == $partSize) {
+                $result .= '</div><div class="col-sm-4">';
+                $this->currentCount = 1;
+            } else {
+                $this->currentCount++;
+            }
 
-        $result .= Html::tag('div', $content, [
-            'class' => 'item',
-            'val' => $row->$valAttribute,
-            'text' => $row->$textAttribute,
-        ]);
 
-        if ($row === $dataProvider->models[count($dataProvider->models) - 1]) {
-            $result .= '</div>';
+            $result .= Html::tag('div', $content, [
+                'class' => 'item',
+                'val' => $row->$valAttribute,
+                'text' => $row->$textAttribute,
+            ]);
+
+            if ($row === $dataProvider->models[count($dataProvider->models) - 1]) {
+                $result .= '</div>';
+            }
+        } else {
+
+            $result = '<div class="col-sm-4 col-xs-6">' . Html::tag('div', $content, [
+                    'class' => 'item',
+                    'val' => $row->$valAttribute,
+                    'text' => $row->$textAttribute,
+                ]) . '</div>';
         }
 
         return $result;
